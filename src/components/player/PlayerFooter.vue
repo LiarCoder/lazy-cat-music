055c8082c4b4d6f5d1c5f6fe2288309b15a55ab7
LiarCoder <liarwhen@qq.com>
2021-11-28 16:41:41 +0800

    feat(layout-player): 完成播放器组件的结构样式开发

---
+++ src/components/player/PlayerFooter.vue
@@ -0,0 +1,122 @@
+<!--
+ * @Description: 描述信息
+ * @Version: 0.1.0
+ * @Author: LiarCoder
+ * @Date: 2021-11-27 15:33:47
+ * @LastEditors: LiarCoder
+ * @LastEditTime: 2021-11-27 19:29:26
+-->
+<template>
+  <div class="player-footer">
+    <van-cell>
+      <template #title>
+        <img
+          src="http://singerimg.kugou.com/uploadpic/softhead/200/20210510/20210510043426122361.jpg"
+          alt=""
+        />
+      </template>
+      <template #value>
+        <p>山随平野尽</p>
+        <p>北鹤</p>
+      </template>
+      <template #right-icon>
+        <div class="player-control-panel">
+          <i class="player-icon-play"></i>
+          <i class="player-icon-next"></i>
+          <i class="player-icon-download"></i>
+        </div>
+      </template>
+    </van-cell>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlayerFooter",
+};
+</script>
+
+<style lang="less">
+.player-footer {
+  .van-cell {
+    padding: 0;
+    width: 100%;
+    height: 4.2143rem;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.9);
+
+    .van-cell__title {
+      flex: none;
+      width: 3.75rem;
+      height: 3.75rem;
+      margin: 0.2143rem 0.535rem 0.2143rem 0.2143rem;
+
+      img {
+        -webkit-tap-highlight-color: transparent;
+        // width: 100%;
+        height: 100%;
+        border-radius: 5px;
+      }
+    }
+    .van-cell__value {
+      // 下面三行是让两个p标签在容器内垂直居中
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      text-align: left;
+      color: #fff;
+      vertical-align: middle;
+      width: 65%;
+      & > p:nth-child(1) {
+        display: block;
+        line-height: 1.7;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        font-size: 0.8571rem;
+      }
+      & > p:nth-child(2) {
+        display: block;
+        line-height: 1.3;
+        font-size: 0.7143rem;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        color: #888;
+      }
+    }
+  }
+  .player-control-panel {
+    position: absolute;
+    top: 1.0714rem;
+    right: 0.7143rem;
+
+    .player-icon-play,
+    .player-icon-next,
+    .player-icon-download {
+      width: 1.786rem;
+      height: 1.786rem;
+      margin-left: 1rem;
+      display: inline-block;
+      vertical-align: middle;
+      cursor: pointer;
+      // animation: loads 1.5s infinite linear;
+    }
+    .player-icon-play {
+      background: url("~@/assets/images/play_icon.png") no-repeat;
+      background-size: 100%;
+    }
+    .player-icon-next {
+      background: url("~@/assets/images/next_icon.png") no-repeat;
+      background-size: 100%;
+    }
+    .player-icon-download {
+      background: url("~@/assets/images/download_icon_player.png") no-repeat;
+      background-size: 100%;
+    }
+  }
+}
+</style>
