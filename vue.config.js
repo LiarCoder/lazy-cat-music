e10b321fb2c2dafc255e5df01ed52e3fb1c6adea
LiarCoder <liarwhen@qq.com>
2021-11-30 20:21:31 +0800

    chore(data-network): 配置代理devServer.proxy

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,26 @@
  * @Author: LiarCoder
  * @Date: 2021-11-19 15:49:40
  * @LastEditors: LiarCoder
- * @LastEditTime: 2021-11-20 19:55:24
+ * @LastEditTime: 2021-11-30 19:53:30
  */
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   lintOnSave: false,
+  devServer: {
+    proxy: {
+      "/m_kugou": {
+        target: "http://m.kugou.com",
+        pathRewrite: { "^/m_kugou": "" },
+        changeOrigin: true,
+        headers: { referer: "http://m.kugou.com" },
+      },
+      "/mobliecdn_kugou": {
+        target: "http://mobilecdn.kugou.com",
+        pathRewrite: { "^/mobliecdn_kugou": "" },
+        changeOrigin: true,
+        headers: { Referer: "http://m.kugou.com" },
+      },
+    },
+  },
 };
